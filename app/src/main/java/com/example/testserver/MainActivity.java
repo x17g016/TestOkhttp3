40a41f924470cd5ba92dd47660d9e4eb193dacb9
WIN_NT\x17g016 <x17g016@chiba-fjb.ac.jp>
2019-11-13 12:05:51 +0900

    パーミッションチェック追加

--- app/src/main/java/com/example/testserver/MainActivity.java
+++ app/src/main/java/com/example/testserver/MainActivity.java
@@ -1,19 +1,42 @@
 package com.example.testserver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener { //①OnClickListener インタフェイスを継承
+
+    static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermission();
         //ActivityのonClickListenerを割り当てる
         findViewById(R.id.button_get).setOnClickListener(this);
         findViewById(R.id.button_post).setOnClickListener(this);
+
+    }
+
+    public void checkPermission(){
+        // パーミッションの許可確認
+        // 許可されていない
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            // パーミッションの許可をリクエスト
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, REQUEST_CODE);
+        }
+        // パーミッションが許可されている
+        else {
+            Toast.makeText(this, "権限が許可されています", Toast.LENGTH_SHORT).show();
+            // 以下通常処理等に飛ばす・・・
+        }
     }
 
     //onClickコールバックをここですべてキャッチして、viewのidで動作を決める
